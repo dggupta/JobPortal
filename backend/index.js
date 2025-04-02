c8cf25c723078b9c17d51fb81fbbf3d23295767f
dggupta <divyanshgupta96302@gmail.com>
2025-04-02 15:49:21 +0530

    final p

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: '*',  // This allows all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials:true
 }
 
 app.use(cors(corsOptions));
